7e4336cec418557232e8c0cd5aca1521e13b7aca
Ritoooo <ritorito.141516@gmail.com>
2019-07-07 12:41:03 -0500

    retrofit ok

---
+++ app/src/main/java/com/example/retrofit3/JSONResponse.java
@@ -0,0 +1,10 @@
+package com.example.retrofit3;
+
+public class JSONResponse {
+    private AndroidVersion[] android;
+
+    public AndroidVersion[] getAndroid() {
+        return android;
+    }
+
+}
